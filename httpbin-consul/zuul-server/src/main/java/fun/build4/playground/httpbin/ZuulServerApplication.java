edefd5457a6065e3fb6fbdacf884660da4672f49
hbc <bcxxxxxx@gmail.com>
2019-09-16 23:30:43 +0800

    feat(httpbin-consul): add servers

---
+++ httpbin-consul/zuul-server/src/main/java/fun/build4/playground/httpbin/ZuulServerApplication.java
@@ -0,0 +1,19 @@
+package fun.build4.playground.httpbin;
+
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulServerApplication {
+
+  public static void main(String[] args) {
+    var app = new SpringApplication(ZuulServerApplication.class);
+    app.setBannerMode(Mode.OFF);
+    app.run(args);
+  }
+}
